62b9cfbec43c6f3aeb0045556e41637cf075678b
Rye Carrigan <rcarrigan@cloudbees.com>
2024-02-29 13:56:34 -0500

    Add same header to script where it was missing

--- ssh-agents/oss-to-cb-shared.groovy
+++ ssh-agents/oss-to-cb-shared.groovy
@@ -1,9 +1,14 @@
+/**
+ * This script is to help users work around BEE-18090, a bug present in CloudBees SSH Build Agents Plugin >= 2.16 and < 2.19
+ *
+ * You will receive compilation errors if both SSH plugins are not present!
+ *
+ * https://docs.cloudbees.com/docs/cloudbees-ci-kb/latest/client-and-managed-masters/why-is-my-ssh-agent-connection-terminated-with-error-server-host-key-rejected
+ */
+
 import com.cloudbees.jenkins.plugins.sshslaves.SSHConnectionDetails
 import com.cloudbees.jenkins.plugins.sshslaves.SSHLauncher as CBLauncher
-import com.cloudbees.jenkins.plugins.sshslaves.verification.*
-import com.cloudbees.opscenter.server.model.SharedSlave
 import hudson.plugins.sshslaves.SSHLauncher as OSSLauncher
-import hudson.plugins.sshslaves.verifiers.*
 import jenkins.model.Jenkins
 
 // Script options.
@@ -16,16 +21,16 @@ def displayEnvironment = false
 
 
 /* Helper method to convert the key strategy from the OSS plugin to CloudBees implementation. */
-static ServerKeyVerificationStrategy convertStrategy(SshHostKeyVerificationStrategy strategy) {
+com.cloudbees.jenkins.plugins.sshslaves.verification.ServerKeyVerificationStrategy convertStrategy(hudson.plugins.sshslaves.verifiers.SshHostKeyVerificationStrategy strategy) {
   switch (strategy) {
-    case NonVerifyingKeyVerificationStrategy:
-      return new BlindTrustConnectionVerificationStrategy()
-    case KnownHostsFileKeyVerificationStrategy:
-      return new KnownHostsConnectionVerificationStrategy()
-    case ManuallyProvidedKeyVerificationStrategy:
-      return new ManuallyConnectionVerificationStrategy(strategy.key)
-    case ManuallyTrustedKeyVerificationStrategy:
-      return new TrustInitialConnectionVerificationStrategy(strategy.requireInitialManualTrust)
+    case hudson.plugins.sshslaves.verifiers.NonVerifyingKeyVerificationStrategy:
+      return new com.cloudbees.jenkins.plugins.sshslaves.verification.BlindTrustConnectionVerificationStrategy()
+    case hudson.plugins.sshslaves.verifiers.KnownHostsFileKeyVerificationStrategy:
+      return new com.cloudbees.jenkins.plugins.sshslaves.verification.KnownHostsConnectionVerificationStrategy()
+    case hudson.plugins.sshslaves.verifiers.ManuallyProvidedKeyVerificationStrategy:
+      return new com.cloudbees.jenkins.plugins.sshslaves.verification.ManuallyConnectionVerificationStrategy(strategy.key)
+    case hudson.plugins.sshslaves.verifiers.ManuallyTrustedKeyVerificationStrategy:
+      return new com.cloudbees.jenkins.plugins.sshslaves.verification.TrustInitialConnectionVerificationStrategy(strategy.requireInitialManualTrust)
     default:
       // There are only the four possible cases so this should be unreachable.
       return null
@@ -33,7 +38,7 @@ static ServerKeyVerificationStrategy convertStrategy(SshHostKeyVerificationStrat
 }
 
 // Iterate over all static agents.
-for (node in Jenkins.get().getAllItems(SharedSlave)) {
+for (node in Jenkins.get().getAllItems(com.cloudbees.opscenter.server.model.SharedSlave)) {
   def launcher = node.getLauncher()
   if (launcher instanceof OSSLauncher) {
     // If launcher is OSS, prepare to convert to CloudBees.
